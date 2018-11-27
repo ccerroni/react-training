f7fc7141afc491c6a1400724cd1d430f24a249c1
Cristian Cerroni <ccerroni@belatrix.com>
2018-11-27 18:07:18 -0300

    deleting tittle

--- src/components/main.js
+++ src/components/main.js
@@ -9,7 +9,6 @@ class Main extends Component {
     return (
       <HashRouter>
         <div>
-          <h1>Simple SPA</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/stuff">Stuff</NavLink></li>
